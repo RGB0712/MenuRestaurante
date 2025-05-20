419c240c59a1c6222402b1dea9396182b30f8805
Raul Gonzalez Barrero <raul.gonzalez.barrero@outlook.com>
2025-05-20 21:02:37 +0200

    visual changes

--- MenuRestaurante_RaulGonzalez/obj/Debug/net8.0/MenuRestaurante_RaulGonzalez.AssemblyInfo.cs
+++ MenuRestaurante_RaulGonzalez/obj/Debug/net8.0/MenuRestaurante_RaulGonzalez.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MenuRestaurante_RaulGonzalez")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24f5ff565216a4492810a311bdeec536ca0b7857")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af2174d0cbd52ae74e786adb14033e1eb3449392")]
 [assembly: System.Reflection.AssemblyProductAttribute("MenuRestaurante_RaulGonzalez")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MenuRestaurante_RaulGonzalez")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
